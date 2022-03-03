00ba1e6aaee47505c70fafe22e687fb42ade9472
Mateusz Grden <matgrden@gmail.com>
2022-03-03 13:02:03 +0100

    import app.css removed and calculateResult added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import "./App.css";
 import React from "react";
 import Container from "./Container";
 import Header from "./Header";
 import Form from "./Form";
 
 function App() {
+  const [result, setResult] = React.useState();
+
+  const calculateResult = (amount, rateInputCurrency, rateOutputCurrency) => {
+    setResult(() => {
+      return (+amount * rateInputCurrency) / rateOutputCurrency;
+    });
+  };
+
   return (
     <Container>
       <Header title="Kantor internetowy" />
-      <Form />
+      <Form calculateResult={calculateResult} result={result} />
     </Container>
   );
 }
