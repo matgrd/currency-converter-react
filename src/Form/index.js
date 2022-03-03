26b1d4410e93c59f81863c5d357e66e704e989e7
Mateusz Grden <matgrden@gmail.com>
2022-03-03 13:03:34 +0100

    amount, inputCurrency, outputCurrency added values

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,31 @@
 import "./style.css";
-import Button from "../Button";
+import React from "react";
+import Button from "./Button";
+import Result from "./Result";
 import { currencies } from "../currencies";
 
-const Form = () => {
+const Form = ({ calculateResult, result }) => {
+  const [amount, setAmount] = React.useState("");
+  const onAmountChange = (event) => setAmount(event.target.value);
+
+  const [inputCurrency, setInputCurrency] = React.useState("Złoty");
+  const onSetInputCurrency = (event) => setInputCurrency(event.target.value);
+
+  const getRateInputCurrency = () =>
+    currencies.find((currency) => currency.name === inputCurrency).rate;
+  const rateInputCurrency = getRateInputCurrency(onSetInputCurrency);
+
+  const [outputCurrency, setOutputCurrency] = React.useState("Złoty");
+  const onSetOutputCurrency = (event) => setOutputCurrency(event.target.value);
+
+  const getRateOutputCurrency = () =>
+    currencies.find((currency) => currency.name === outputCurrency).rate;
+  const rateOutputCurrency = getRateOutputCurrency(onSetOutputCurrency);
+
   const onFormSubmit = (event) => {
     event.preventDefault();
+
+    calculateResult(amount, rateInputCurrency, rateOutputCurrency);
   };
 
   return (
@@ -21,15 +42,21 @@ const Form = () => {
               type="number"
               step="1"
               min="1"
+              value={amount}
+              onChange={onAmountChange}
             />
           </label>
         </p>
         <p className="form__paragraph">
           <label>
             <span className="form_labelText">Przelicz z:</span>
-            <select name="currency" className="form__select">
+            <select
+              name="currency"
+              className="form__select"
+              onChange={onSetInputCurrency}
+            >
               {currencies.map((curency) => (
-                <option key={curency.shortName} value={curency.shortName}>
+                <option key={curency.shortName} value={curency.name}>
                   {curency.name}
                 </option>
               ))}
@@ -39,19 +66,20 @@ const Form = () => {
         <p className="form__paragraph">
           <label>
             <span className="form_labelText">Przelicz na:</span>
-            <select name="currency" className="form__select">
+            <select
+              name="currency"
+              className="form__select"
+              onChange={onSetOutputCurrency}
+            >
               {currencies.map((curency) => (
-                <option key={curency.shortName} value={curency.shortName}>
+                <option key={curency.shortName} value={curency.name}>
                   {curency.name}
                 </option>
               ))}
             </select>
           </label>
         </p>
-        <p className="form__paragraph form__paragraph--modified">
-          Po przewalutowaniu otrzymasz:
-          <span className="form__result"> N/A</span>
-        </p>
+        <Result result={result} />
       </fieldset>
       <Button />
     </form>
